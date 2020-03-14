a1cef8dad692a529cb0fe0bb206d041fa269bc06
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2020-03-13 18:19:19 -0700

    Use sacred for keeping track of the experiments

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
 # Tutorial from: https://fenicsproject.org/docs/dolfin/2019.1.0/python/demos/stokes-iterative/demo_stokes-iterative.py.html
+from sacred import Experiment, SETTINGS
+from sacred.observers import FileStorageObserver, MongoObserver
+from sacred.utils import apply_backspaces_and_linefeeds
 from dolfin import *
 import mshr
 import sys
@@ -8,13 +11,29 @@ from rectangle import solve_rectangle
 import numpy as np
 import sys
 
-AR = 1.
-L = 3
-res = 50
-bar_width=.1
-krylov_method = "minres" ## alternatively use tfqrm
+ex = Experiment('Diagonal FEM')
+ex.observers.append(MongoObserver.create())
+SETTINGS.CAPTURE_MODE = 'sys'
+ex.captured_out_filter = apply_backspaces_and_linefeeds
+
+@ex.config
+def cfg():
+    ## Geometry parameters
+    AR = 1.
+    L = 3
+    res = 50
+    bar_width=.1
+    krylov_method = "minres" ## alternatively use tfqrm ## TODO use it in the integrator
+
+    ## Fluid parameters
+    mu=1. ## TODO use it in the integrator
 
 
-u, p = solve_rectangle(AR,L,res,bar_width)
-
-plot_fluid(u, L=L, plot_type='quiver')
+@ex.automain
+def main(_config):
+    AR = _config['AR']
+    L = _config['L']
+    res = _config['res']
+    bar_width = _config['bar_width']
+    u, p = solve_rectangle(AR,L,res,bar_width)
+    plot_fluid(u, L=L, plot_type='quiver')
